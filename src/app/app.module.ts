9a707d57f2f6c36264db536f5213fff554855f7a
Romain1664 <roomain.s.31@gmail.com>
2020-05-20 14:20:12 +0200

    premiers lien

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ReglesComponent } from './regles/regles.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { JouerComponent } from './jouer/jouer.component';
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'regles', component: ReglesComponent },
+  { path: 'jouer', component: JouerComponent },
+  { path: 'connexion', component: ConnexionComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: HomeComponent }
+];
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ReglesComponent,
+    JouerComponent,
+    ConnexionComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/jouer/jouer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jouer',
+  templateUrl: './jouer.component.html',
+  styleUrls: ['./jouer.component.css']
+})
+export class JouerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
